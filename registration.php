a9cb9b9c7b475789f5503a67c0d303a2cecf01f7
N0n3 <52095116+N0n3-github@users.noreply.github.com>
2019-07-19 12:52:07 +0500

    Update registration.php

--- registration.php
+++ registration.php
@@ -43,6 +43,7 @@
 <html>
 <head>
 	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=0.5">
     <link rel="stylesheet" href="src/bootstrap/css/bootstrap.css">
     <link rel="stylesheet" href="src/css/registration.css">
 	<title>IT Exam</title>
