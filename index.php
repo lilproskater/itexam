fc92568400e4159ee76dca13f8d056fdcf13617f
N0n3 <52095116+N0n3-github@users.noreply.github.com>
2019-07-19 12:53:48 +0500

    Update index.php

--- index.php
+++ index.php
@@ -30,7 +30,7 @@
 <html>
 <head>
 	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="viewport" content="width=device-width, initial-scale=0.5">
 	<link rel="stylesheet" href="src/bootstrap/css/bootstrap.css">
 	<link rel="stylesheet" href="src/css/style.css">
 	<title>IT Exam</title>
