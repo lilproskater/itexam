d69b2af1ac8aef343358052ec7008f5da6b0b35d
N0n3 <52095116+N0n3-github@users.noreply.github.com>
2019-07-20 12:51:15 +0500

    Update exam.php

--- exam.php
+++ exam.php
@@ -1,5 +1,7 @@
 <?php
   require "./config.php";
+  $questions = R::findAll('questions');
+  $question_number = 1;
 ?>
 
 <!DOCTYPE HTML>
@@ -18,14 +20,14 @@
 	</script>
     <div class="container-fluid">
         <div class="row">
-            <div class="col-md-6 info-container fixed-col">
+            <div class="col-md-6 col-sm-6 info-container fixed-col">
             	<div class="info">
                     Имя: <?= $_SESSION['logged_user']->name; ?> <br>
                     Фамилия: <?= $_SESSION['logged_user']->surname; ?> <br>
                     Класс: <?= $_SESSION['logged_user']->grade, $_SESSION['logged_user']->letter; ?> <br>
                 </div>
             </div>
-            <div class="col-md-6 timer-container fixed-col">
+            <div class="col-md-6 col-sm-6 timer-container fixed-col">
                 <div id="timer">
                     <!-- Timer here -->
                     03 : 00 : 00
@@ -34,17 +36,18 @@
         </div>
         <div class="row">
             <div class="col-md-12 content">
-                <p><b>1. Question 1 ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</b></p>
-                <p>A) 50 B) 20 C) 30 D) 40</p>
-                <input type="radio" class="radio" name="Q1"><font class="font">A</font><br>
-  		<input type="radio" class="radio" name="Q1"><font class="font">B</font><br>
- 		<input type="radio" class="radio" name="Q1"><font class="font">C</font><br>  
- 		<input type="radio" class="radio" name="Q1"><font class="font">D</font><br> 
+                <?php
+                    foreach ($questions as $question) {
+                    	echo '<p><b>'.$question_number.'. '.$question->question.'</b></p>';
+                    	echo '<p> A) '.$question->a.' B) '.$question->b.' C) '.$question->c.' D) '.$question->d.'</p>';
+                    	echo '<input type="radio" class="radio" name="Q'.$question_number.'" value="A"><font class="font">A</font><br>';
+                    	echo '<input type="radio" class="radio" name="Q'.$question_number.'" value="B"><font class="font">B</font><br>';
+                    	echo '<input type="radio" class="radio" name="Q'.$question_number.'" value="C"><font class="font">C</font><br>';
+                    	echo '<input type="radio" class="radio" name="Q'.$question_number.'" value="D"><font class="font">D</font><br>';
+                    	echo '<br><br>';
+                    	$question_number ++;
+                    }
+                ?>
             </div>
         </div>
     </div>
