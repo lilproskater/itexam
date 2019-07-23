c71ed7e0eb7b457d9d064ca19aee491123ce5670
N0n3 <52095116+N0n3-github@users.noreply.github.com>
2019-07-23 15:43:12 +0500

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,5 +1,8 @@
 <?php
     require './config.php';
+    $user = R::findOne('profiles', 'username = ?', array($_SESSION['logged_user']->username));
+    $user->status = "offline";
+    R::store($user);
     unset($_SESSION['logged_user']);
     header('Location: /');
 ?>
