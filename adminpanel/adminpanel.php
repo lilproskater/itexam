197099f2c55058b7594e1a079698f6f036ce336f
N0n3 <52095116+N0n3-github@users.noreply.github.com>
2019-08-03 21:55:09 +0500

    Update adminpanel.php

--- adminpanel/adminpanel.php
+++ adminpanel/adminpanel.php
@@ -4,10 +4,10 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-	<meta charset="UTF-8">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=0.5">
     <link rel="stylesheet" href="../src/bootstrap/css/bootstrap.css">
-	<title>IT Exam Adminpanel</title>
+    <title>IT Exam Adminpanel</title>
 </head>
 <body>
     Вы вошли админ!
