e1cee6a19c1d8d14182382531ae6d6f465b248db
N0n3 <52095116+N0n3-github@users.noreply.github.com>
2019-08-04 00:10:14 +0500

    Update adminpanel.php

--- adminpanel/adminpanel.php
+++ adminpanel/adminpanel.php
@@ -1,5 +1,9 @@
 <?php 
     require __DIR__.'/../config.php';
+    $data = $_POST;
+    if (isset($data['do_logout'])) {
+        header('Location: ./logout.php');
+    }
 ?>
 <!DOCTYPE HTML>
 <html>
@@ -11,9 +15,15 @@
 </head>
 <body>
     <?php if (!isset($_SESSION['logged_admin'])): ?>
-        Хей админ, авторизируйся)
+        <form action="./">
+            <h1>Хей админ, авторизируйся)</h1>
+            <button type="submit">На главную админку =)</button>
+        </form>
     <?php else: ?>
-        Вы вошли админ!
+        <form action="./adminpanel.php" method="POST">
+            <h1>Вы вошли админ!</h1>
+            <button type="submit" name="do_logout">Выход</button>
+        </form>
     <?php endif; ?>
 </body>
 </html>
