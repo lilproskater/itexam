03433019765949e232d952c0d9b8fab89fc2c8b0
N0n3 <52095116+N0n3-github@users.noreply.github.com>
2019-08-03 21:08:17 +0500

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
     require 'src/libs/rb.php';
-    R::setup('mysql:host=localhost;dbname=itexam', 'uname', 'upass');
+    R::setup('mysql:host=localhost;dbname=itexam', 'uname', 'upassss');
 
     if(!R::testConnection()){
         echo "DB is not connected! Check your connection again!";
