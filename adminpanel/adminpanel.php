9be7aae6c9c38cc4525562ec8ed2c83e64c43d36
N0n3 <52095116+N0n3-github@users.noreply.github.com>
2019-08-04 12:38:24 +0500

    Update adminpanel.php

--- adminpanel/adminpanel.php
+++ adminpanel/adminpanel.php
@@ -1,29 +1,48 @@
 <?php 
     require __DIR__.'/../config.php';
     $data = $_POST;
-    if (isset($data['do_logout'])) {
-        header('Location: ./logout.php');
-    }
 ?>
+
 <!DOCTYPE HTML>
 <html>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=0.5">
     <link rel="stylesheet" href="../src/bootstrap/css/bootstrap.css">
-    <title>IT Exam Adminpanel</title>
+    <link rel="stylesheet" href="../src/css/adminpanel.css">
+    <title>IT Exam Admin panel</title>
 </head>
 <body>
     <?php if (!isset($_SESSION['logged_admin'])): ?>
-        <form action="./">
-            <h1>Хей админ, авторизируйся)</h1>
-            <button type="submit">На главную админку =)</button>
-        </form>
+        <div class="container ooops">
+            <form action="./">
+                <h1 class="title">Хей админ, авторизируйся)</h1>
+                <button class="btn btn-success exit-btn" type="submit">На главную админку =)</button>
+            </form>
+        </div>
     <?php else: ?>
-        <form action="./adminpanel.php" method="POST">
-            <h1>Вы вошли админ!</h1>
-            <button type="submit" name="do_logout">Выход</button>
-        </form>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-6 col-sm-6 header-info">
+                    <div class="inner-info">
+                        <h1 class="header-title">Админ панель</h1>
+                        <font class="admin-uname-text"><?= ucfirst(strtolower($_SESSION['logged_admin']->username)) ?></font>
+                    </div>
+                </div>
+                <div class="col-md-6 col-sm-6 header-right">
+                    <form action="./logout.php">
+                        <button type="submit" class="btn btn-success logout-btn">Выход</button>
+                    </form>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-2 col-sm-2 sidebar">
+                    <button class="btn-success btn sidebar-btn">Вопросы</button><br>
+                    <button class="btn-success btn sidebar-btn">Профили</button><br>
+                    <button class="btn-success btn sidebar-btn">Результаты</button><br>
+                </div>
+            </div>
+        </div>
     <?php endif; ?>
 </body>
 </html>
